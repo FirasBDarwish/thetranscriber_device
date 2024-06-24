5c9a40188e1ebe82ce6ca4f7cc7660410e528adf
FirasBDarwish <darwish.firas.b@gmail.com>
2024-06-24 12:56:45 +0400

    added .pbmm and streaming.py file

--- getting_devices.py
+++ getting_devices.py
@@ -8,39 +8,39 @@ for i in range(p.get_device_count()):
 
 p.terminate()
 
-import numpy as np
+# import numpy as np
 
-CHUNK = 1024  # Number of frames per buffer
+# CHUNK = 1024  # Number of frames per buffer
 
-def callback(in_data, frame_count, time_info, status):
-    # Process audio data here (optional)
-    return (in_data, pyaudio.paContinue)
+# def callback(in_data, frame_count, time_info, status):
+#     # Process audio data here (optional)
+#     return (in_data, pyaudio.paContinue)
 
-p = pyaudio.PyAudio()
+# p = pyaudio.PyAudio()
 
-# Open stream with Soundflower as input
-stream = p.open(format=pyaudio.paInt16,
-                channels=2,  # Adjust based on Soundflower configuration
-                rate=44100,  # Sample rate (adjust if necessary)
-                input=True,
-                input_device_index=4,  # Replace with Soundflower device index
-                frames_per_buffer=CHUNK,
-                stream_callback=callback)
+# # Open stream with Soundflower as input
+# stream = p.open(format=pyaudio.paInt16,
+#                 channels=2,  # Adjust based on Soundflower configuration
+#                 rate=44100,  # Sample rate (adjust if necessary)
+#                 input=True,
+#                 input_device_index=4,  # Replace with Soundflower device index
+#                 frames_per_buffer=CHUNK,
+#                 stream_callback=callback)
 
-print("Capturing audio from Soundflower. Press Ctrl+C to stop.")
+# print("Capturing audio from Soundflower. Press Ctrl+C to stop.")
 
-# Start stream
-stream.start_stream()
+# # Start stream
+# stream.start_stream()
 
-try:
-    while stream.is_active():
-        pass
-except KeyboardInterrupt:
-    print("Stopping...")
+# try:
+#     while stream.is_active():
+#         pass
+# except KeyboardInterrupt:
+#     print("Stopping...")
 
-# Stop stream
-stream.stop_stream()
-stream.close()
+# # Stop stream
+# stream.stop_stream()
+# stream.close()
 
-# Terminate PyAudio
-p.terminate()
+# # Terminate PyAudio
+# p.terminate()
